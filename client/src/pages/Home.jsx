969a91c9aeafece9d093ab772fd0f563b6ecef87
ipanropi <irfan100053@gmail.com>
2024-02-17 13:07:31 -0600

    Add pagination, and fix responsive design

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,9 +3,13 @@ import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
 import {Link} from "react-router-dom";
 import axios from "axios";
+import Pagination from "../components/Pagination.jsx";
 
 const Home = () => {
     const [posts, setPosts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [startIndex, setStartIndex] = useState(0);
+    const length = Math.ceil(posts.length / 6);
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -15,23 +19,29 @@ const Home = () => {
         fetchPosts();
     }, []);
 
-
     return (
-            <div className="grid md:grid-cols-2 gap-4">
-                {posts.map(({post_id, title, content, to_char}) => (
+        <>
+            <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-12 md:gap-6 pt-8 lg:p-8 min-h-screen">
+                {posts.slice(startIndex, startIndex + 6).map(({post_id, title, content, to_char}) => (
                     <Link to={`/post/${post_id}`} key={post_id}
-                          className="flex flex-col justify-center items-center min-h-72 p-4 gap-2  transition duration-150 ease-out hover:-translate-y-1 hover:scale-105 hover:bg-gray-200 hover:rounded-md">
-                        <img
-                            src="https://images.squarespace-cdn.com/content/v1/624b3c6d692bd24ce26121ca/1649097848557-WK8UT5I9K54FX8569LNW/03_20160720S1_WEATHERBY_ANGLERS_160.jpg?format=2500w"
-                            alt="picture"/>
-                        <div className="flex flex-col p-0  w-full">
+                          className="border border-gray-200 rounded-lg overflow-hidden aspect-[9/14] md:aspect-square flex flex-col justify-center items-center transition duration-150 ease-out hover:-translate-y-1 hover:shadow-2xl">
+                        <div className="flex flex-1 w-full justify-center items-center border-b border-gray-200 bg-[#F0F0F0]">
+                            <h1 className="text-lg text-center p-4">
+                                {content.split(" ").slice(0, 10).join(" ")}
+                            </h1>
+                        </div>
+                        <div className="flex flex-col pl-4 pr-4 pb-4 w-full h-2/5 md:h-1/3">
                             <h3 className="text-xs pt-2">{to_char}</h3>
-                            <h1 className="text-xl font-bold pt-2 capitalize">{title}</h1>
+                            <h1 className="text-lg md:text-xl font-bold pt-2 capitalize">{title}</h1>
                             <p className="text-sm pt-2 first-letter:capitalize whitespace-pre-line line-clamp-4">{content}</p>
                         </div>
                     </Link>
                 ))}
             </div>
+            <Pagination length={length} setCurrentPage={setCurrentPage} currentPage={currentPage} setStartIndex={setStartIndex}/>
+        </>
+
+
     );
 };
 
