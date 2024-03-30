7fcc1a88ddfe2fa1f582a7a7e10b0167cc4b415c
ipanropi <irfan100053@gmail.com>
2024-03-29 22:31:47 -0500

    Resolve share post_id bug

--- client/src/components/Share.jsx
+++ client/src/components/Share.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLocation } from "react-router-dom";
 
-const Share = (post_id) => {
+const Share = ({post_id}) => {
     const location = useLocation();
 
     const isShareSupported = navigator.share;
